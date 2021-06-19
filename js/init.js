7e66c685c98c809b9870429ce3b9a3b3c651d6f6
nubewebt <nubewebt@gmail.com>
2021-06-19 16:54:13 -0500

    Actualización02

--- js/init.js
+++ js/init.js
@@ -9,10 +9,10 @@
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyCd-3Ukxu-a9KABoJhJ_WXVoOimMTCaZjM",
-    authDomain: "nubewebt-a1723.firebaseapp.com",
-    projectId: "nubewebt-a1723",
-    storageBucket: "nubewebt-a1723.appspot.com",
-    messagingSenderId: "1054684732317",
-    appId: "1:1054684732317:web:9e02785a10489a8642ed74",
-    measurementId: "G-FCB81HCMLJ"
+  authDomain: "nubewebt-a1723.firebaseapp.com",
+  projectId: "nubewebt-a1723",
+  storageBucket: "nubewebt-a1723.appspot.com",
+  messagingSenderId: "1054684732317",
+  appId: "1:1054684732317:web:9e02785a10489a8642ed74",
+  measurementId: "G-FCB81HCMLJ"
 });
